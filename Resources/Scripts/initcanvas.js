87c0cda5c6a410fa9078b600459d3d1465ffcef2
Jackson Luff <gamerzbis@hotmail.com>
2015-11-17 22:21:32 +1100

    I fixed it.. I Fixed It.. .I FIXED IT! IT WORKS NOW! :D
    Background generation is a go!

---
+++ Resources/Scripts/initcanvas.js
@@ -0,0 +1,39 @@
+var settings;
+
+window.addEventListener('load', function(){
+    initCanvas(settings);
+});
+
+window.addEventListener('resize', function(){
+    initCanvas(settings);
+});
+ 
+function initCanvas(settings){
+    settings = loadSettings(settings);
+    //init trianglify
+    var pattern = Trianglify(settings);
+    //add trianglify to #backgroundCanvas
+    pattern.canvas(document.getElementById('backgroundCanvas'));
+}
+
+function loadSettings(settings){
+   
+   var width = document.documentElement.offsetWidth; 
+   var height = document.documentElement.offsetHeight; 
+   
+    var settings = {
+      cell_size: 75,
+      variance: 0.75,
+      x_colors: 'random',
+      y_colors: 'match_x',
+      palette: Trianglify.colorbrewer,
+      color_space: 'lab',
+      color_function: false,
+      stroke_width: 1.51,
+      width: width,
+      height: height,
+      seed: null
+    };
+ 
+    return settings;
+}
