d4721dce53cefacce36b01f3afd9dfe987371548
Jackson Luff <gamerzbis@hotmail.com>
2015-11-17 16:39:28 +1100

    Added completely new skin.
    Shaders now have viewing gif's and websites!

--- Resources/Deps/General.js
+++ Resources/Deps/General.js
@@ -1,3 +1,107 @@
-$(document).ready(function()          
+$(window).ready(function()
 {
+    $("#Shader_ProGen").hover(
+        function()
+        {
+            $("#ProGen_Img").attr("src", "Resources/Images/Shaders/ProGen.gif");
+        },
+        function()
+        {
+            $("#ProGen_Img").attr("src", "Resources/Images/Projects/GenGein_thumb1.jpg");
+        }
+    );
+    
+    /// ================== INTERACTIVE TEXTURS CHUNK =======================
+    
+    $("#Shader_InteractV1").hover(
+        function()
+        {
+            $("#InteractV1_Img").attr("src", "Resources/Images/Shaders/InteractV1.gif");
+        },
+        function()
+        {
+            $("#InteractV1_Img").attr("src", "Resources/Images/Shaders/InteractV1.jpg");
+        }
+    );
+    
+    $("#Shader_InteractV2").hover(
+        function()
+        {
+            $("#InteractV2_Img").attr("src", "Resources/Images/Shaders/InteractV2.gif");
+        },
+        function()
+        {
+            $("#InteractV2_Img").attr("src", "Resources/Images/Shaders/InteractV2.jpg");
+        }
+    );
+    
+    $("#Shader_InteractV3").hover(
+        function()
+        {
+            $("#InteractV3_Img").attr("src", "Resources/Images/Shaders/InteractV3.gif");
+        },
+        function()
+        {
+            $("#InteractV3_Img").attr("src", "Resources/Images/Shaders/InteractV3.jpg");
+        }
+    );
+    
+    $("#Shader_InteractV4").hover(
+        function()
+        {
+            $("#InteractV4_Img").attr("src", "Resources/Images/Shaders/InteractV4.gif");
+        },
+        function()
+        {
+            $("#InteractV4_Img").attr("src", "Resources/Images/Shaders/InteractV4.jpg");
+        }
+    );
+    
+    /// ================== PARTICLES CHUNK =======================
+    
+    $("#Shader_Particles_Spiral").hover(
+        function()
+        {
+            $("#Particles_Spiral_Img").attr("src", "Resources/Images/Shaders/Particles_Spiral.gif");
+        },
+        function()
+        {
+            $("#Particles_Spiral_Img").attr("src", "Resources/Images/Shaders/Particles_Spiral.jpg");
+        }
+    );
+    
+    $("#Shader_Particles_Morph").hover(
+        function()
+        {
+            $("#Particles_Morph_Img").attr("src", "Resources/Images/Shaders/Particles_Morph.gif");
+        },
+        function()
+        {
+            $("#Particles_Morph_Img").attr("src", "Resources/Images/Shaders/Particles_Morph.jpg");
+        }
+    );
+    
+    /// ================== MISC CHUNK =======================
+    
+    $("#Shader_Water").hover(
+        function()
+        {
+            $("#Water_Img").attr("src", "Resources/Images/Shaders/Water.gif");
+        },
+        function()
+        {
+            $("#Water_Img").attr("src", "Resources/Images/Shaders/Water.jpg");
+        }
+    );
+    
+    $("#Shader_WatchDogs").hover(
+        function()
+        {
+            $("#WatchDogs_Img").attr("src", "Resources/Images/Shaders/WatchDogs.gif");
+        },
+        function()
+        {
+            $("#WatchDogs_Img").attr("src", "Resources/Images/Shaders/WatchDogs.jpg");
+        }
+    );
 });
